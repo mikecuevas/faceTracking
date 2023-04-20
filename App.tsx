7ff0a8e3c89ae8613329b24432eff266c147cbbf
Mike <mikefabiosauercuevas@gmail.com>
2023-04-19 22:40:24 -0300

    initial code with the face tracking

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import { Home } from './src/Home';
+
+export default function App() {
+  return (
+    <Home />
+  );
+}
---
+++ src/Home/style.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flexGrow: 1,
+      //backgroundColor: '#fff',
+    },
+    camera: {
+        flex: 1,
+    }, 
+  });
+  
